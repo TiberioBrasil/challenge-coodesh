1c5afebce594abd997c39bedf19d7466cc94aba9
Tiberio Brasil <tiberiobrasil@gmail.com>
2021-03-06 09:39:55 -0300

    :art: add nest config, moment, class validator, pg and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+        database: process.env.DATABASE_NAME,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
+    ConfigModule.forRoot({
+      ignoreEnvFile: process.env.NODE_ENV === 'production' ? true : false,
+      envFilePath: process.env.NODE_ENV !== 'production' ? '.env' : null,
+      load: [appConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
